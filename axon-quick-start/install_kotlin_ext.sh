f85a60fb1eb4f1228c084e43e821dcc295b6d7e4
Milen Dyankov <milendyankov@gmail.com>
2021-10-11 13:58:02 +0200

    Install Kotlin VSCode ext

---
+++ axon-quick-start/install_kotlin_ext.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /tmp && apt-get update -y && apt install libarchive-tools -y # install bsdtar
+
+curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/fwcd/vsextensions/kotlin/0.2.23/vspackage | bsdtar -xvf - extension
+
+mv extension /opt/.katacodacode/extensions/fwcd.kotlin-0.2.23
